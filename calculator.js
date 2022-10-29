0d37eb9cbf1405d6f9083504617d2547db82885a
skoptelin <warziher@yandex.ru>
2022-10-29 21:09:27 +0300

    add mobile burger-menu + some change on calculator

--- calculator.js
+++ calculator.js
@@ -3,73 +3,139 @@ let secondNumber;
 let operation;
 let result;
 
-//Калькулятор
-function calculate() {
-    getNumber1();
-    getOperationItem();
-    getNumber2();
-
-    //Преобразование переменных из строкового в числовые значения
-    let firstConvertNumber = Number(firstNumber);
-    let secondConvertNumber = Number(secondNumber);
-
-    if(operation === "+") {
-        result = (firstConvertNumber + secondConvertNumber).toFixed(2);
-
-        return result;
-    }
-    if(operation === "-") {
-        result = (firstConvertNumber - secondConvertNumber).toFixed(2);
-
-        return result;
-    }
-    if(operation === "*") {
-        result = (firstConvertNumber * secondConvertNumber).toFixed(2);
-
-        return result;
-    }
-    if(operation === "/") {
-        result = (firstConvertNumber / secondConvertNumber).toFixed(2);
-        return result;
-    }
-}
-
 //Вывод алерта с результатом калькулятора
 function showResult() {
-    if ((document.getElementById("enterBox1").value !== "") && (document.getElementById("operation").value !== "") && (document.getElementById("enterBox2").value !== "")) { //Проверка, чтобы поля ввода не были пустыми
-        calculate(firstNumber, secondNumber, operation);
-        let elementWindow = document.querySelector(".resultBox");
+    let elementInput1  = document.querySelector("#enterBox1");
+    let elementInput2  = document.querySelector("#enterBox2");
+    let operationInput = document.querySelector("#operation");
+    let elementWindow  = document.querySelector(".resultBox");
+    let resultText1    = document.querySelector(".resultText");
+    let resultText2    = document.getElementById("resultText");
+    if ((elementInput1.value !== "") && (operationInput.value !== "") && (elementInput2.value !== "")) { //Проверка, чтобы поля ввода не были пустыми
+        calculateAjax(firstNumber, secondNumber, operation);
         elementWindow.classList.remove("hiddenResultBox");
-        document.querySelector(".resultText").innerHTML = result;
-        document.getElementById("resultText").innerHTML = result;
+    } else { //Если поля ввода пустые, просим заполнить поля и подсвечиваем их красным
+        elementInput1.classList.remove("enterBox1");
+        elementInput2.classList.remove("enterBox2");
+        operationInput.classList.remove("enterBoxWithButton");
+        elementWindow.classList.remove("hiddenResultBox");
+        resultText1.innerHTML = "Введите, пожалуйста, первое число, операцию и второе число";
+        resultText2.innerHTML = "Введите, пожалуйста, первое число, операцию и второе число";
     }
 }
 
-//Скрытие алерта
+//Скрытие алерта с результатом калькулятора
 function hide() {
     let elementWindow = document.querySelector(".resultBox");
     elementWindow.classList.add("hiddenResultBox");
 }
 
-//Забираем первое число
+//Забираем первое число из enterBox1
 function getNumber1() {
-    let elementInput = document.querySelector(".enterBox1");
-    firstNumber = elementInput.value;
+    let elementInput1 = document.querySelector("#enterBox1");
+    firstNumber = elementInput1.value;
     return firstNumber;
 }
 
-//Забираем знак операции
+//Забираем знак операции из operation
 function getOperationItem() {
-    let elementInput = document.getElementById("operation");
-    operation = elementInput.value;
+    let operationInput = document.querySelector("#operation");
+    operation = operationInput.value;
     return operation;
 }
 
-//Забираем второе число
+//Забираем второе число из enterBox2
 function getNumber2() {
-    let elementInput = document.querySelector(".enterBox2");
-    secondNumber = elementInput.value;
+    let elementInput2 = document.querySelector("#enterBox2");
+    secondNumber = elementInput2.value;
     return secondNumber;
+}
+
+//Показываем/скрываем меню бургера, только для мобильной версии
+function showHideNav() {
+    let showBurgerMenu = document.querySelector (".showNavList");
+    showBurgerMenu.classList.toggle("navList");
+}
+
+// Ограниение ввода символов в инпутах калькулятора
+function maxInputElements() {
+    let elementInput1 = document.querySelector("#enterBox1");
+    let elementInput2 = document.querySelector("#enterBox2");
+
+    elementInput1.oninput = function() {
+        this.value = this.value.substr(0, 16);
+    }
+    elementInput2.oninput = function() {
+        this.value = this.value.substr(0, 16);
+    }
+}
+
+// Убираем красную подсветку полей калькулятора при вводе значений
+function delIncorInput() {
+    let elementInput1 = document.querySelector("#enterBox1");
+    let elementInput2 = document.querySelector("#enterBox2");
+    let operationInput = document.querySelector("#operation");
+
+    let valueInput1 = elementInput1.value;
+    let valueInput2 = elementInput2.value;
+    let operInput = operationInput.value;
+    
+    if (valueInput1 != "") {
+        elementInput1.classList.remove("enterBox1Invalid");
+        elementInput1.classList.add("enterBox1");
+    } else {
+        elementInput1.classList.remove("enterBox1");
+        elementInput1.classList.add("enterBox1Invalid");
+    }
+
+    if (valueInput2 != "") {
+        elementInput2.classList.remove("enterBox2Invalid");
+        elementInput2.classList.add("enterBox2");
+    } else {
+        elementInput2.classList.remove("enterBox2");
+        elementInput2.classList.add("enterBox2Invalid");
+    }
+
+    if (operInput != "") {
+        operationInput.classList.remove("enterBoxWithButtonInvalid");
+        operationInput.classList.add("enterBoxWithButton");
+    } else {
+        operationInput.classList.remove("enterBoxWithButton");
+        operationInput.classList.add("enterBoxWithButtonInvalid");
+    }
+}
+
+//Калькулятор через Ajax запрос
+function calculateAjax() {
+
+    getNumber1();
+    getOperationItem();
+    getNumber2();
+
+    let firstConvertedNumber = Number(firstNumber);
+    let secondConvertedNumber = Number(secondNumber);
+    let resultText1 = document.querySelector(".resultText");
+    let resultText2 = document.getElementById("resultText");
+
+    if(operation === "+"){
+        operation = "%2B";
+    }
+
+    fetch("calculator.php?firstNumber=" + firstConvertedNumber + "&operation=" + operation + "&secondNumber=" + secondConvertedNumber)
+    .then(respose => respose.json())
+    .then(jsonObject => {
+        resultText1.innerHTML = jsonObject.result;
+        resultText2.innerHTML = jsonObject.result;
+
+        let i;
+        let operationHistory = "";
+
+        for(i = 0; i < jsonObject.operationHistory.length; i++) {
+            let arr = jsonObject.operationHistory[i];
+            operationHistory = operationHistory + "--> " + arr.first_number + " " + arr.operator + " " + arr.second_number + " = " + arr.result + "; <br/>";
+        }
+        document.getElementById("operationHistory").innerHTML = operationHistory;
+    })
 }
 
 
--- calculator.php
+++ calculator.php
@@ -1,40 +1,36 @@
 <?php
-
-    $connection = mysqli_connect("localhost", "root", "", "calculator");
+    // Подключение к БД
+    $connection = mysqli_connect("localhost", "root", "", "calculator"); 
     if (!$connection) {
         die ("Связь не установлена: " . mysqli_connect_error());
     }
 
-    $query = mysqli_query($connection, "SELECT * FROM calc_base ORDER BY id DESC LIMIT 7");
+    // Подключение к БД + SQL запрос на выбор последних 7 строк в БД
+    $query = mysqli_query($connection, "SELECT * FROM calc_base ORDER BY id DESC LIMIT 7"); 
 
     $operations = [];
 
-    while ($row = mysqli_fetch_assoc($query)){
-        
+    //Цикл для заполнения переменной operations из БД
+    while ($row = mysqli_fetch_assoc($query)){ 
         $operations[] = $row;
     }
 
-    if(isset($_REQUEST["firstNumber"]) && !empty($_REQUEST["operation"]) && isset($_REQUEST["secondNumber"])) {
+    // Калькулятор
+    if(isset($_REQUEST["firstNumber"]) && !empty($_REQUEST["operation"]) && isset($_REQUEST["secondNumber"])) { 
         $firstNumber = $_REQUEST["firstNumber"];
         $operation = $_REQUEST["operation"];
         $secondNumber = $_REQUEST["secondNumber"];
 
         if ($operation === "+") {
             $result = $firstNumber + $secondNumber;
-
-            mysqli_query($connection, "INSERT INTO calc_base (first_number, operator, second_number, result) VALUES ($firstNumber, '" . $_REQUEST["operation"] . "', $secondNumber, $result)");
         }
 
         if ($operation === "-") {
             $result = $firstNumber - $secondNumber;
-
-            mysqli_query($connection, "INSERT INTO calc_base (first_number, operator, second_number, result) VALUES ($firstNumber, '" . $_REQUEST["operation"] . "', $secondNumber, $result)");
         }
         
         if ($operation === "*") {
             $result = $firstNumber * $secondNumber;
-
-            mysqli_query($connection, "INSERT INTO calc_base (first_number, operator, second_number, result) VALUES ($firstNumber, '" . $_REQUEST["operation"] . "', $secondNumber, $result)");
         }
 
         if ($operation === "/") {
@@ -42,66 +38,17 @@
                 $result = "Ошибка! На 0 делить нельзя.";
             } else {
                 $result = $firstNumber / $secondNumber;
-                mysqli_query($connection, "INSERT INTO calc_base (first_number, operator, second_number, result) VALUES ($firstNumber, '" . $_REQUEST["operation"] . "', $secondNumber, $result)");
             }
         }
+        if($secondNumber != "0") {
+            mysqli_query($connection, "INSERT INTO calc_base (first_number, operator, second_number, result) VALUES ($firstNumber, '" . $operation . "', $secondNumber, $result)");
+        }
     }
+
+    //Ответ сервера в формате JSON
+    $response = [
+        "result" => $result,
+        "operationHistory" => $operations
+    ];
+    echo json_encode($response);
 ?>
-
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>Калькулятор</div>
-    <form method="POST">
-        <div>Введите первое число:</div>
-        <input type="number" name="firstNumber" step="0.001" required> 
-        <div>Выберите операцию:</div>
-            <select name="operation" required>
-                <option>
-
-                </option>
-                <option value="+">
-                    + Сложить
-                </option>
-                <option value="-">
-                    - Вычесть
-                </option>
-                <option value="*">
-                    * Умножить
-                </option>
-                <option value="/">
-                    / Разделить
-                </option> 
-            </select>
-        <div>Введите второе число:</div>
-        <input type="number" name="secondNumber" step="0.001" required>
-        <input type="submit" value="Вычислить" name="resultButton">
-    </form>
-    <?php if (isset ($result)) { ?>    
-        <div>
-            Ваш результат: <?php echo $result; ?>
-        </div>
-    <?php } ?>
-
-    <?php if (!empty($operations)) { ?>
-        <div>
-            История операций: 
-        </div>
-
-        <?php foreach ($operations as $operation_list) { ?>
-            <div>
-                <span>-> [<?php echo $operation_list["id"]; ?>]</span> 
-                <?php echo $operation_list["first_number"] . $operation_list["operator"] . $operation_list["second_number"]; ?> 
-                <span>=</span>
-                <?php echo $operation_list["result"]; ?>
-            </div>
-        <?php } ?>
-    <?php } ?>
-</body>
-</html>
